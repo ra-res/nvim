48a427df33418e0b517350442f6f2e161a33062f
ra-res <ra-res@users.noreply.github.com>
2022-04-15 20:06:25 +0100

    feat(plugins): add colorizer

---
+++ lua/ra-res/colorizer.lua
@@ -0,0 +1,13 @@
+local status_ok, colorizer = pcall(require, "colorizer")
+
+if not status_ok then
+  return
+end
+
+colorizer.setup {
+  'css';
+  'javascript';
+  html = {
+    mode = 'foreground';
+  }
+}
