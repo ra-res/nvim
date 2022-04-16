e73941d09bb6c3b024434c92759acb9f12e3125b
ra-res <ra-res@users.noreply.github.com>
2022-04-16 14:31:04 +0100

    chore(mappings): move plugins key mappings to mappings lua file

--- lua/ra-res/mappings.lua
+++ lua/ra-res/mappings.lua
@@ -30,8 +30,8 @@ keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
 keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
 -- Navigate buffers
-keymap("n", "<S-l>", ":bnext<CR>", opts)
-keymap("n", "<S-h>", ":bprevious<CR>", opts)
+keymap("n", "<Tab>", "<cmd>bnext<CR>", opts)
+keymap("n", "<S-Tab>", "<cmd>bprevious<CR>", opts)
 
 -- Navigate tabs
 keymap('n', 'tn', ':tabnew<CR>', opts)
