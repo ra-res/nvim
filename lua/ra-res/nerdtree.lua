47305d88b09bc0d1418a9d6b75a6267bdeedb50c
nifty <purtan.rares@yahoo.com>
2022-04-16 14:31:40 +0100

    feat(nvim-tree): replace nerdtree with nvim-tree

--- lua/ra-res/nerdtree.lua
+++
@@ -1,7 +0,0 @@
-local keymap = vim.api.nvim_set_keymap
-opts = { noremap = true } 
-
-keymap('n', '<C-t>', ':NERDTreeToggle<CR>', opts)
-keymap('n', '<C-f>', ':NERDTreeFind<CR>', opts)
-
-vim.g.NERDTreeQuitOnOpen = true
