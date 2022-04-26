f536957a040c97ce22654690b0f28527b724a4ad
ra-res <ra-res@users.noreply.github.com>
2022-04-27 00:14:29 +0100

    feat(lsp-signature): add plugin

--- lua/ra-res/plugins.lua
+++ lua/ra-res/plugins.lua
@@ -66,6 +66,7 @@ packer.startup(function(use)
   use "williamboman/nvim-lsp-installer" -- simple to use language server installer
   use "tamago324/nlsp-settings.nvim" -- language server settings defined in json for
   use "jose-elias-alvarez/null-ls.nvim" -- for formatters and linters
+  use "ray-x/lsp_signature.nvim"
 
   -- Theme/Syntax
   use 'kyazdani42/nvim-web-devicons'
